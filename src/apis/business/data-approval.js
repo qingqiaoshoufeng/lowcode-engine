694d7fb7a258b7384bf9998e604fcb8d636e0226
hexiaofei <hexiaofei@hxdi.com>
2023-11-05 09:12:43 +0800

    feat: 新增警情待更正审批

---
+++ src/apis/business/data-approval.js
@@ -0,0 +1,47 @@
+import { request } from '@/plugins/axios/index.js'
+import dayjs from 'dayjs'
+
+// 警情更正/修改申请列表
+export function getFireWarningEditApproval(data) {
+  return request.get('/acws/rest/biz/warning/reviewlist/query', {
+    params: {
+      offset: (data.page - 1) * data.limit + 1,
+      limit: data.limit,
+      recheckType: data.recheckType,
+      startDate: data.time && data.time[0] ? dayjs(data.time[0]).format('YYYY-MM-DD HH:mm:ss') : '',
+      endDate: data.time && data.time[1] ? dayjs(data.time[1]).format('YYYY-MM-DD HH:mm:ss') : '',
+      state: data.state,
+      applyType: data.applyType,
+      applyGroup: data.applyGroup?.length > 0 ? data.applyGroup[0].organizationid : '',
+      warningCode: data.warningCode,
+      dispatchCode: data.dispatchCode,
+      fireCode: data.fireCode,
+    },
+  }).then((res) => {
+    return {
+      ...res,
+      list: res.items,
+    }
+  })
+}
+
+// 警情作废申请列表
+export function getFireWarningAbolishApproval(data) {
+  return request.get('/acws/rest/biz/warning/cancelreviewlist/query', {
+    params: {
+      offset: (data.page - 1) * data.limit + 1,
+      limit: data.limit,
+      startDate: data.time && data.time[0] ? dayjs(data.time[0]).format('YYYY-MM-DD HH:mm:ss') : '',
+      endDate: data.time && data.time[1] ? dayjs(data.time[1]).format('YYYY-MM-DD HH:mm:ss') : '',
+      state: data.state,
+      applyType: data.applyType,
+      applyGroup: data.applyGroup?.length > 0 ? data.applyGroup[0].organizationid : '',
+      warningCode: data.warningCode,
+    },
+  }).then((res) => {
+    return {
+      ...res,
+      list: res.items,
+    }
+  })
+}
---
+++ src/views/policeRecheckApproval/index.vue
@@ -0,0 +1,307 @@
+<script setup>
+import { ref, onMounted, nextTick } from "vue";
+import ProList from "@/component/ProList/index";
+import SelectTime from "@/component/SelectTime/index";
+import SelectMore from "@/component/SelectMore/index";
+import ProModal from "@/component/ProModal/index";
+import PoliceEntryDetail from '@/views/policeEntryDetail/index.vue';
+import {
+  checkAbolishState,
+  generateColorByState,
+  getLastMonth,
+} from "@/utils/tools.js";
+import { MSG_LOCKING_TEXT, applyType } from '@/utils/constants.js';
+import { showToast, showLoadingToast, closeToast } from "vant";
+import { getFireWarningEditApproval } from "@/apis/index.js";
+import { formatYmdHm } from "@/utils/format.js";
+import { useModal } from '@/hooks/useModal.js'
+
+const searchOptions = ref([
+  {
+    title: '选择时间',
+    type: 'select-range',
+    placeholder: '请选择时间',
+    value: 'time',
+  },
+  {
+    title: '警情编号',
+    type: 'input',
+    placeholder: '请输入警情编号',
+    value: "warningCode",
+  },
+  {
+    title: '申请单位',
+    type: 'select-org',
+    placeholder: '请选择申请单位',
+    params: { permission: true },
+    single: true,
+    selectLeaf: false,
+    headersDisabled: true,
+    value: 'orgIds',
+  },
+  {
+    title: '申请类型',
+    type: 'select-single',
+    placeholder: '请选择申请类型',
+    options: [],
+    fieldNames: { value: 'value', label: 'label' },
+    value: 'applyType',
+  },
+])
+
+const defaultFilterValue = {
+  state: 'running',
+  time: getLastMonth(),
+};
+
+const tabs = ref([
+  {
+    title: "待审批",
+    value: 'running',
+  },
+  {
+    title: "已审批",
+    value: 'completed',
+  },
+]);
+
+const { show } = useModal();
+
+const currentRow = ref(null);
+
+const proListRef = ref(null);
+
+const onTabFn = (name, title) => {
+  if (title === tabs.value[1].title) {
+    proListRef.value.query = defaultFilterValue
+    proListRef.value.query.state = 'completed';
+    showLoadingToast();
+    proListRef.value.filter().then(() => {
+      closeToast();
+    });
+  } else if (title === tabs.value[0].title) {
+    proListRef.value.query = defaultFilterValue
+    proListRef.value.query.state = 'running';
+    showLoadingToast();
+    proListRef.value.filter().then(() => {
+      closeToast();
+    });
+  }
+};
+
+const handleApproval = (row) => {
+  if (row.isLock === '1') {
+    showToast(MSG_LOCKING_TEXT)
+    return
+  }
+  currentRow.value = row
+  show.value.reviewVisible = true
+};
+
+const handleItem = (row) => {
+  currentRow.value = row
+  show.value.lookVisible = true
+};
+
+const onTimeChange = (value) => {
+  showLoadingToast();
+  proListRef.value.filter().then((res) => {
+    closeToast();
+  });
+};
+
+const onSearchConfirm = () => {
+  showLoadingToast();
+  proListRef.value.filter().then((res) => {
+    closeToast();
+  });
+}
+
+const finishCallback = () => {
+  proListRef.value.filter()
+}
+
+onMounted(() => {
+  searchOptions.value[3].options = applyType
+  nextTick(() => {
+    proListRef.value?.filter();
+  });
+});
+</script>
+
+<template>
+  <div class="police-recheck-approval">
+    <ProList
+      ref="proListRef"
+      title="警情更正审批"
+      :defaultFilterValue="defaultFilterValue"
+      :getListFn="getFireWarningEditApproval"
+      :tabs="tabs"
+      rowKey="boFireWarningId"
+      :showLoad="false"
+      :onTabFn="onTabFn"
+    >
+      <template #search="{ filterFormState, resetForm }">
+        <div class="list-tabs">
+          <SelectTime
+            v-model:value="filterFormState.time"
+            title="选择时间"
+            @change="onTimeChange"
+          />
+          <SelectMore
+            :options="searchOptions"
+            :reset-fn="resetForm"
+            @confirmCallback="onSearchConfirm"
+          />
+        </div>
+      </template>
+      <template #list="{ record }">
+        <div class="list-item" @click="handleItem(record)">
+          <div class="item-header">
+            <div class="item-title">{{ record.warningName }}</div>
+            <div class="item-state" :class="generateColorByState(record.warningStatusValue)">
+              {{ record.warningStatusValue }}
+            </div>
+          </div>
+          <div class="item-type">
+            <span>{{ record.warningTypeValue }}</span>
+          </div>
+          <div class="item-field">
+            <img src="../../assets/images/icon-time@2x.png" alt="" />
+            <div style="color: #929398">接警时间：</div>
+            <div>{{ formatYmdHm(record.warningDate) }}</div>
+          </div>
+          <div class="item-field">
+            <img
+              src="../../assets/images/icon-area@2x.png"
+              style="width: 13px; height: 15px; margin-right: 8px"
+              alt=""
+            />
+            <div style="color: #929398">行政区域：</div>
+            <div>{{ record.warningAreaValue }}</div>
+          </div>
+          <div class="item-line" />
+          <div class="item-operate" @click.stop>
+            <van-button
+              v-p="['admin', 'police-manage:abolish']"
+              type="success"
+              size="mini"
+              color="#1989fa"
+              class="item-btn"
+              @click="handleApproval(record)"
+              :disabled="!checkAbolishState(record.warningStatusValue, record.updatePermission)"
+            >
+              审批
+            </van-button>
+          </div>
+        </div>
+      </template>
+    </ProList>
+
+    <!-- 警情详情 -->
+    <ProModal v-model:visible="show.lookVisible" :showBack="true" :showHeader="false" title="警情详情">
+      <PoliceEntryDetail :current-row="currentRow" />
+    </ProModal>
+    <!-- 警情更正审批 -->
+    <ProModal v-model:visible="show.reviewVisible" title="警情更正审批">
+      <template #default="{ setHandleOk }">
+        <PoliceEntryDetail
+          :current-row="currentRow"
+          :is-approval="true"
+          process-key="applyEditFlow"
+          :set-handle-ok="setHandleOk"
+          @finish-callback="finishCallback"
+        />
+      </template>
+    </ProModal>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.police-recheck-approval {
+  height: 100%;
+  background-color: #f6f7f8;
+  .list-tabs {
+    display: flex;
+    padding: 10px 16px 0 16px;
+  }
+  .list-item {
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    margin-top: 10px;
+    .item-header {
+      display: flex;
+      padding: 8px 10px;
+      .item-title {
+        width: 260px;
+        font-size: 16px;
+        font-weight: bold;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      .item-state {
+        width: 57px;
+        height: 24px;
+        font-size: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 2px;
+        margin-left: auto;
+      }
+    }
+    .item-field {
+      font-size: 14px;
+      color: #1f1f1f;
+      display: flex;
+      align-items: center;
+      padding: 0 0 8px 10px;
+      img {
+        width: 14px;
+        height: 14px;
+        margin-right: 6px;
+      }
+    }
+    .item-type {
+      margin: 0 0 8px 10px;
+      span {
+        display: inline-block;
+        font-size: 12px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #fc2902;
+        background: #ffefec;
+        border-radius: 2px;
+        padding: 4px 10px;
+      }
+    }
+    .item-line {
+      width: 100%;
+      border-top: 1px solid rgba(31, 31, 31, 0.15);
+    }
+    .item-operate {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      padding: 8px 10px;
+      .item-collect {
+        font-size: 20px;
+        margin-right: auto;
+      }
+      .item-btn {
+        padding: 0 16px;
+        margin-left: 10px;
+        :deep(.van-button__content) {
+          height: 18px;
+        }
+        :deep(.van-button__text) {
+          white-space: nowrap;
+          word-break: break-all;
+        }
+      }
+    }
+  }
+}
+</style>
