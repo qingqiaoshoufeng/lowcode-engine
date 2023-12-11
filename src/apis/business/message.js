8f73c27f56c29cbb3baa883f3fbeb20d66373d6e
yuzhiqiang <yuzhiqiang2@hxdi.com>
2023-12-11 14:43:50 +0800

    feat: 去除重名方法-重要标注

--- src/apis/business/message.js
+++ src/apis/business/message.js
@@ -2,9 +2,9 @@
 import { request as axios } from '@/plugins/axios/index.js'
 
 //  我的消息--已读
-export function updateMessage(params) {
-  return axios.get('/acws/rest/biz/firestatistics/usermessage/update', { params })
-}
+// export function updateMessage(params) {
+//   return axios.get('/acws/rest/biz/firestatistics/usermessage/update', { params })
+// }
 
 export function getMessageInfo(params) {
   const {page,limit,...rest} = params || {}
--- src/apis/business/report-review.js
+++ src/apis/business/report-review.js
@@ -24,26 +24,26 @@ export function getDispatchReviewList(data) {
 }
 
 // 火灾审核列表
-export function getFireReviewList(data) {
-  return request.get('/acws/rest/biz/report/reviewlist/query', {
-    params: {
-      offset: (data.page - 1) * data.limit + 1,
-      limit: data.limit,
-      state: data.state,
-      reviewType: '2',
-      sendTimeStart: data.time && data.time[0] ? dayjs(data.time[0]).format('YYYY-MM-DD HH:mm:ss') : '',
-      sendTimeEnd: data.time && data.time[1] ? dayjs(data.time[1]).format('YYYY-MM-DD HH:mm:ss') : '',
-      fireCode: data.fireCode,
-      fireGroup: data.fireGroup?.length > 0 ? data.fireGroup[0].organizationid : '',
-      fireStatus: data.fireStatus?.join(','),
-    },
-  }).then((res) => {
-    return {
-      ...res,
-      list: res.items,
-    }
-  })
-}
+// export function getFireReviewList(data) {
+//   return request.get('/acws/rest/biz/report/reviewlist/query', {
+//     params: {
+//       offset: (data.page - 1) * data.limit + 1,
+//       limit: data.limit,
+//       state: data.state,
+//       reviewType: '2',
+//       sendTimeStart: data.time && data.time[0] ? dayjs(data.time[0]).format('YYYY-MM-DD HH:mm:ss') : '',
+//       sendTimeEnd: data.time && data.time[1] ? dayjs(data.time[1]).format('YYYY-MM-DD HH:mm:ss') : '',
+//       fireCode: data.fireCode,
+//       fireGroup: data.fireGroup?.length > 0 ? data.fireGroup[0].organizationid : '',
+//       fireStatus: data.fireStatus?.join(','),
+//     },
+//   }).then((res) => {
+//     return {
+//       ...res,
+//       list: res.items,
+//     }
+//   })
+// }
 
 // 审核初始化信息
 export function getInitProcess(data) {
