40f25ad988a8b4ff211d5a0d0ee7422ebaa1854b
hexiaofei <hexiaofei@hxdi.com>
2023-12-19 17:05:26 +0800

    fix: 火灾填报校验规则修改

--- src/views/fire-report/config/tool.js
+++ src/views/fire-report/config/tool.js
@@ -4,10 +4,10 @@ import store from '@/store/index.js'
 console.log(store,'store11');
 import { showToast } from 'vant';
 export const checkArray = (arr) => {
-  let result = false;
+  let result = true;
   ['工地', '居住场所', '物资仓储场所', '厂房'].forEach((item) => {
     if (arr.join(',').includes(`${item},`)) {
-      result = true
+      result = false
     }
   })
   return result
@@ -24,10 +24,10 @@ export const checkFireResistanceRating = (form, options, prompt = true) => {
   const filter = options.buildStructure?.filter(item => item.boDictId === buildStructure.value)
   const rate = options.fireResistanceRating?.filter(item => item.boDictId === fireResistanceRating.value)
   // 建筑结构
-  if (filter?.[0]?.dictName === '钢砼结构' && (rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  if (filter?.[0]?.dictName === '钢砼结构' && ['三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
   }
-  else if (filter?.[0]?.dictName === '砖混结构' && rate?.[0]?.dictName === '四级') {
+  else if (filter?.[0]?.dictName === '砖混结构' && ['四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
   }
   else if (filter?.[0]?.dictName === '砖木结构' && (rate?.[0]?.dictName === '一级' || rate?.[0]?.dictName === '二级')) {
@@ -46,45 +46,45 @@ export const checkFireResistanceRating = (form, options, prompt = true) => {
     form.fireBuilding.fireResistanceRating.warningText = `建筑结构为${filter?.[0]?.dictName}，耐火等级选择不合理，请修改或备注！`
     form.fireBuilding.fieldWarning = form.fireBuilding.fieldWarning.replace('fireResistanceRating:false;', 'fireResistanceRating:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: `建筑结构为${filter?.[0]?.dictName}，耐火等级选择不合理，请修改或备注！`, style: { backgroundColor: 'orange' } })
-    showToast(`建筑结构为${filter?.[0]?.dictName}，耐火等级选择不合理，请修改或备注！`)
+    prompt && showToast(`建筑结构为${filter?.[0]?.dictName}，耐火等级选择不合理，请修改或备注！`)
   }
   // 起火场所、建筑楼层
   const types = options.buildType?.filter(item => item.boDictId === buildType.value)
-  if (firePlace?.text?.indexOf('居住场所') > -1 && buildFloor.value >= 19 && (rate?.[0]?.dictName === '二级' || rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  if (firePlace?.text?.indexOf('居住场所') > -1 && buildFloor.value >= 19 && ['二级', '三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '居住场所，19<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '居住场所，19<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('居住场所，19<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '居住场所，19<=总楼层数，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '居住场所，19<=总楼层数，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
-  else if (firePlace?.text?.indexOf('居住场所') > -1 && (buildFloor.value < 19 && buildFloor.value >= 10) && (rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  else if (firePlace?.text?.indexOf('居住场所') > -1 && (buildFloor.value < 19 && buildFloor.value >= 10) && ['三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '居住场所，10<=总楼层数<19，耐火等级选择三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '居住场所，10<=总楼层数<19，耐火等级选择三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('居住场所，10<=总楼层数<19，耐火等级选择三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '居住场所，10<=总楼层数<19，耐火等级选择三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '居住场所，10<=总楼层数<19，耐火等级选择三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
-  else if (checkArray(firePlace?.text) && buildFloor.value >= 17 && (rate?.[0]?.dictName === '二级' || rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  else if (checkArray(firePlace?.text) && buildFloor.value >= 17 && ['二级', '三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '17<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '17<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('17<=总楼层数，耐火等级选择二、三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '17<=总楼层数，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '17<=总楼层数，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
-  else if (checkArray(firePlace?.text) && (buildFloor.value >= 9 && buildFloor.value < 17) && (rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  else if (checkArray(firePlace?.text) && (buildFloor.value >= 9 && buildFloor.value < 17) && ['三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '9<=总楼层数<17，耐火等级选择二、三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '9<=总楼层数<17，耐火等级选择二、三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('9<=总楼层数<17，耐火等级选择二、三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '9<=总楼层数<17，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '9<=总楼层数<17，耐火等级选择二、三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
-  else if (firePlace?.text?.indexOf('厂房') > -1 && types?.[0]?.dictName === '高层' && (rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  else if (firePlace?.text?.indexOf('厂房') > -1 && types?.[0]?.dictName === '高层' && ['三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '厂房火灾，建筑类别为高层时，选择三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '厂房火灾，建筑类别为高层时，选择三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('厂房火灾，建筑类别为高层时，选择三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '厂房火灾，建筑类别为高层时，选择三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '厂房火灾，建筑类别为高层时，选择三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
-  else if (types?.[0]?.dictName === '地下' && (rate?.[0]?.dictName === '二级' || rate?.[0]?.dictName === '三级' || rate?.[0]?.dictName === '四级')) {
+  else if (types?.[0]?.dictName === '地下' && ['二级', '三级', '四级', '低于四级'].includes(rate?.[0]?.dictName)) {
     form.fireBuilding.fireResistanceRating.warning = true
-    form.fireBuilding.fireResistanceRating.warningText = '建筑类别为地下室，选择二、三、四级不合理，请修改或备注！'
-    // prompt && notification.open({ message: '填报异常提醒', description: '建筑类别为地下室，选择二、三、四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('建筑类别为地下室，选择二、三、四级不合理，请修改或备注！')
+    form.fireBuilding.fireResistanceRating.warningText = '建筑类别为地下室，选择二、三、四级、低于四级不合理，请修改或备注！'
+    // prompt && notification.open({ message: '填报异常提醒', description: '建筑类别为地下室，选择二、三、四级、低于四级不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast(form.fireBuilding.fireResistanceRating.warningText)
   }
   if (form.fireBuilding.fireResistanceRating.warning === true) {
     form.fireBuilding.fieldWarning = form.fireBuilding.fieldWarning.replace('fireResistanceRating:false;', 'fireResistanceRating:true;')
@@ -98,6 +98,7 @@ export const checkAffectedHouse = (form, prompt = true) => {
     form.economicLoss.affectedHouse.warning = true
     form.economicLoss.fieldWarning = form.economicLoss.fieldWarning.replace('affectedHouse:false;', 'affectedHouse:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: '非轻微火灾，居住场所受灾户数为0不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
+    prompt && showToast('非轻微火灾，居住场所受灾户数为0不合理，请修改或备注！')
   }
   else {
     form.economicLoss.affectedHouse.warning = false
@@ -114,7 +115,7 @@ export const checkFireInjuryCost = (form, prompt = true) => {
     form.economicLoss.fireInjuryCost.warning = true
     form.economicLoss.fieldWarning = form.economicLoss.fieldWarning.replace('fireInjuryCost:false;', 'fireInjuryCost:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: '无人员伤亡，人身伤亡支出费用应该为0，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('无人员伤亡，人身伤亡支出费用应该为0，请修改或备注！')
+    prompt && showToast('无人员伤亡，人身伤亡支出费用应该为0，请修改或备注！')
   }
   else {
     form.economicLoss.fireInjuryCost.warning = false
@@ -129,7 +130,7 @@ export const checkFireDisposalCost = (form, prompt = true) => {
     form.economicLoss.fireDisposalCost.warning = true
     form.economicLoss.fieldWarning = form.economicLoss.fieldWarning.replace('fireDisposalCost:false;', 'fireDisposalCost:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: '非轻微火灾，现场处置费用为0，不合理，请修改或备注！', style: { backgroundColor: 'orange' } })
-    showToast('非轻微火灾，现场处置费用为0，不合理，请修改或备注！')
+    prompt && showToast('非轻微火灾，现场处置费用为0，不合理，请修改或备注！')
   }
   else {
     form.economicLoss.fireDisposalCost.warning = false
@@ -148,7 +149,7 @@ export const checkBurnedArea = (form, prompt = true) => {
     form.basicInfo.burnedArea.warningText = `当火灾为轻微火灾时，建构筑物火灾的过火面积合理范围为${severityConfig?.[0]?.value?.[0]}-${severityConfig?.[0]?.value?.[1]}平方米`
     form.basicInfo.fieldWarning = form.basicInfo.fieldWarning.replace('burnedArea:false;', 'burnedArea:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: form.basicInfo.burnedArea.warningText, style: { backgroundColor: 'orange' } })
-    showToast(form.basicInfo.burnedArea.warningText)
+    prompt && showToast(form.basicInfo.burnedArea.warningText)
   }
   else if ((Number(burnedArea.value) < severityConfig?.[1]?.value?.[0] || Number(burnedArea.value > severityConfig?.[1]?.value?.[1]))
   && severity.value === '1' && !fireType?.text?.includes('建构筑物火灾')) {
@@ -156,7 +157,7 @@ export const checkBurnedArea = (form, prompt = true) => {
     form.basicInfo.burnedArea.warningText = `当火灾为轻微火灾时，非建构筑物火灾的过火面积合理范围为${severityConfig?.[1]?.value?.[0]}-${severityConfig?.[1]?.value?.[1]}平方米`
     form.basicInfo.fieldWarning = form.basicInfo.fieldWarning.replace('burnedArea:false;', 'burnedArea:true;')
     // prompt && notification.open({ message: '填报异常提醒', description: form.basicInfo.burnedArea.warningText, style: { backgroundColor: 'orange' } })
-    showToast( form.basicInfo.burnedArea.warningText)
+    prompt && showToast( form.basicInfo.burnedArea.warningText)
   }
   else {
     form.basicInfo.burnedArea.warning = false
