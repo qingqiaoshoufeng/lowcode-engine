c7a226875dfeda6c433d8e9a42f3658e4c0baf9f
yangchuan <yangchuan@hxdi.com>
2024-04-24 10:18:34 +0800

    fix: 火灾简况字体HZTJ-1275

--- src/views/fire-report/components/fireInfoTransferList_brief-situation.vue
+++ src/views/fire-report/components/fireInfoTransferList_brief-situation.vue
@@ -1,23 +1,23 @@
 <script setup>
-import { inject } from 'vue'
-import { gutter } from '@/utils/constants.js'
+import { inject } from "vue";
+import { gutter } from "@/utils/constants.js";
 
-const form = inject('form')
+const form = inject("form");
 </script>
 
 <template>
-    <van-cell-group>
+  <van-cell-group>
     <div :gutter="gutter">
       <div :span="24" class="fz16">
-        {{ form.briefSituation.content.value}}
+        {{ form.briefSituation.content.value }}
       </div>
     </div>
   </van-cell-group>
 </template>
 <style lang="scss" scoped>
-.fz16{
-  font-size: 16px;
+.fz16 {
+  font-size: var(--van-cell-font-size);
+  color: rgb(50, 50, 51);
   padding: 10px 16px;
 }
 </style>
-
