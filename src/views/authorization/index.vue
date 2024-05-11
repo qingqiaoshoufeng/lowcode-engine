ae1321c5d9d19ca8a623f6e8656058eb7713e27d
hexiaofei <hexiaofei@hxdi.com>
2024-05-11 16:16:45 +0800

    feat: 新增授权登录页

---
+++ src/views/authorization/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="authorization-wrapper">
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { showLoadingToast } from "vant";
+
+onMounted(() => {
+  showLoadingToast({
+    message: '授权认证中...',
+    duration: 0,
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+.authorization-wrapper {
+  width: 100vw;
+  height: 100vh;
+  background-color: #fff;
+}
+</style>
