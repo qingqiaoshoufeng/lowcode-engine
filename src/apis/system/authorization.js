8643446fcf4a18034defa48916ae76283524a60c
hexiaofei <hexiaofei@hxdi.com>
2024-05-23 10:36:38 +0800

    fix: 去掉多余内容

--- src/apis/system/authorization.js
+++ src/apis/system/authorization.js
@@ -1,10 +1,5 @@
 import { request as axios } from '@/plugins/axios/index.js'
 
-// 获取各省简称
-export function getSimpleName(organizationid) {
-  return axios.get(`/acws/rest/biz/common/sysmap/${organizationid}`)
-}
-
 // 开始授权认证
 export function startAuthorization(data) {
   return axios.post('/acws/rest/biz/firetruck/saveOrUpdate', data)
