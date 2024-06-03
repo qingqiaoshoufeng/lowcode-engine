d5b90dc7c166fa623782a024fe504166f957ae37
hexiaofei <hexiaofei@hxdi.com>
2024-06-03 14:20:14 +0800

    fix: cordova 网络状态事件修改

--- src/hooks/useCordovaINetwork.js
+++ src/hooks/useCordovaINetwork.js
@@ -1,30 +1,25 @@
 import { onMounted, onUnmounted, ref } from 'vue'
 
 export const useCordovaINetwork = (initial) => {
-  const networkState = ref('')
+  const networkState = ref('online')
 
-  const onDeviceReady = () => {
+  onMounted(() => {
     // 当网络状态发生变化时，更新网络状态
-    if (window?.cordova?.plugins?.network) {
-      window?.cordova?.plugins?.network?.addEventListener('online', () => {
+    if (window.cordova) {
+      document.addEventListener('online', () => {
         networkState.value = 'online';
       });
-      window?.cordova?.plugins?.network?.addEventListener('offline', () => {
+      document.addEventListener('offline', () => {
         networkState.value = 'offline';
       });
     }
-  }
-
-  onMounted(() => {
-    document.addEventListener('deviceready', onDeviceReady, false);
   })
 
   onUnmounted(() => {
     // 清理事件监听器
-    document.removeEventListener('deviceready', onDeviceReady, false);
-    if (window?.cordova?.plugins?.network) {
-      window?.cordova?.plugins?.network?.removeEventListener('online');
-      window?.cordova?.plugins?.network?.removeEventListener('offline');
+    if (window.cordova) {
+      document.removeEventListener('online');
+      document.removeEventListener('offline');
     }
   })
 
