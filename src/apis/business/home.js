54384714915145d78682679095cf0deafb5cb4ba
hexiaofei <hexiaofei@hxdi.com>
2024-06-07 09:28:30 +0800

    fix: 路径修改

--- src/apis/business/home.js
+++ src/apis/business/home.js
@@ -4,45 +4,45 @@ import { request as axios } from '@/plugins/axios/index.js'
 
 // 查询页面信息
 export function getDataAnalysis(params) {
-  return axios.get('acws/rest/biz/analysis/dataAnalysis/homePage/list', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/list', { params })
 }
 
 // 火灾相关信息
 export function getFirelist(params) {
-  return axios.get('acws/rest/biz/analysis/dataAnalysis/homePage/firelist', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/firelist', { params })
 }
 
 // 获取通知列表
 export function getFireNotice(params) {
-  return axios.get('acws/rest/biz/mystatistics/notice/query', { params }).then((res)=>{
+  return axios.get('/acws/rest/biz/mystatistics/notice/query', { params }).then((res)=>{
     return res.items
   })
 }
 
 // 榜单列表
 export function getAnnualList(params) {
-  return axios.get('acws/rest/biz/analysis/dataAnalysis/homePage/annualList', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/annualList', { params })
 }
 
 // 查询出动信息接口
 export function getDispatchInfo(params) {
-  return axios.get('acws/rest/biz/analysis/dataAnalysis/homePage/dispatch', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/dispatch', { params })
 }
 
 // 火灾平时常
 export function getAverageTime(params) {
-  return axios.get('acws/rest/biz/analysis/dataAnalysis/homePage/averageTime', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/averageTime', { params })
 }
 
 //参展形式接口
 export function getStylePercent(params) {
-  return axios.get('acws/rest//biz/analysis/dataAnalysis/homePage/parameter', { params })
+  return axios.get('/acws/rest/biz/analysis/dataAnalysis/homePage/parameter', { params })
 }
 
 // 获取通知详情
 export function getNoticeDetail(params) {
   const {id} = params
-  return axios.get(`acws/rest/biz/notice/${id}`)
+  return axios.get(`/acws/rest/biz/notice/${id}`)
 }
 
 // 下载附件
