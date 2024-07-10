3db97be7872db7f0d3b2ce5667aee962ce1abb56
hexiaofei <hexiaofei@hxdi.com>
2024-07-10 10:48:23 +0800

    fix: 修改版本号

--- src/plugins/axios/interceptors.js
+++ src/plugins/axios/interceptors.js
@@ -8,7 +8,7 @@ import { whiteList } from './white.js'
 export const requestInterceptors = (config) => {
   config.headers.token = localStorage.token
   config.headers.platform = localStorage.platform || 'App'
-  config.headers['client-version'] = '1.3.7-1-20240711'
+  config.headers['client-version'] = '1.3.7-1-20240710'
   return config
 }
 
